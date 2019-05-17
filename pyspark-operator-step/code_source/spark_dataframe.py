671720ea093321888afb7ede4b5ff87e1460abbc
J20 <LYJ__h@163.com>
2019-05-17 14:32:04 +0800

    update

--- pyspark-operator-step/code_source/spark_dataframe.py
+++ pyspark-operator-step/code_source/spark_dataframe.py
@@ -6,13 +6,14 @@ from pyspark.sql import Column
 import pandas as pd
 import numpy as np
 
-# 连接到Spark 集群
+# 创建SparkSession连接到Spark集群-SparkSession.builder.appName('name').getOrCreate()
 spark=SparkSession \
 .builder \
 .appName('my_app_name') \
 .getOrCreate()
 
 # 创建DataFrame,可以从不同的数据创建，以下进行对个数据源读取创建说明
+
 def create_json_file():
     df=pd.DataFrame(np.random.rand(5,5),columns=['a','b','c','d','e']).applymap(lambda x: int(x*10))
     file=r"random.csv"
@@ -25,7 +26,7 @@ def create_df_from_rdd():
                     (456, "Michael", 22, "green"),
                     (789, "Simone", 23, "blue")])
 
-    # 设置dataFrame将要使用的数据模型，定义列名，类型和是否为空
+    # 设置dataFrame将要使用的数据模型，定义列名，类型和是否为能为空
     schema = StructType([StructField("id", LongType(), True),
                         StructField("name", StringType(), True),
                         StructField("age", LongType(), True),
@@ -48,6 +49,9 @@ def create_df_from_rdd():
     print("{}{}".format("swimmer numbers : ",swimmers.count()) )
 
 def create_df_from_json():
+    '''
+    read的类型是DataFrameReader
+    '''
     df = spark.read.json('pandainfo.json')
     df.show()
 
@@ -55,11 +59,38 @@ def create_df_from_csv():
     df=spark.read.csv('random.csv',header=True, inferSchema=True)
     df.show()
 
-def create_df_from_db(db):
-    pass
+def create_df_from_postgres():
+    """
+    format : 指定数据源格式 - 如 jdbc ， json ， csv等
+    options: 为数据源添加相关特性选项
+    """
+    df=spark.read.format('jdbc').options(
+        url='jdbc:postgresql://localhost:5432/northwind',
+        dbtable='public.orders',
+        user='postgres',
+        password='iamroot'
+    ).load()
 
-def create_df_from_pandas(pandas):
-    pass
+    df.show()
+
+def create_df_from_mysql():
+    """
+    """
+    df=spark.read.format('jdbc').options(
+        url='jdbc:mysql://localhost:3306',
+        dbtable='mysql.db',
+        user='root',
+        password='iamneo'
+        ).load()
+    df.show()
+
+def create_df_from_pandas():
+    """
+    从Python pandas获取数据
+    """
+    df = pd.DataFrame(np.random.random((4,4)))
+    spark_df = spark.createDataFrame (df,schema=['a','b','c','d'])
+    spark_df.show()
 
 def create_df_from_parquet(parquet):
     pass
@@ -72,5 +103,8 @@ def create_df_from_hive(hive):
 if __name__=='__main__':
     #create_json_file()
     #create_df_from_rdd() 
-    create_df_from_csv()
-    create_df_from_json()
+    #create_df_from_csv()
+    #create_df_from_json()
+    #create_df_from_db()
+    create_df_from_mysql()
+    #create_df_from_pandas()
