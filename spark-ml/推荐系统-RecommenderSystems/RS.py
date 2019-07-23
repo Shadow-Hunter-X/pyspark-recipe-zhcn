4516f22725a527f629f041f682b07e80066b552e
J20 <LYJ__h@163.com>
2019-07-23 18:01:11 +0800

    update

---
+++ spark-ml/推荐系统-RecommenderSystems/RS.py
@@ -0,0 +1,34 @@
+from pyspark.sql import SparkSession 
+spark=SparkSession.builder.appName('rs').getOrCreate()
+
+from pyspark.sql.functions import *
+
+print('-------------查看数据情况，检测数据质量和相关的特征。即相对数据有一定的认识，对后续进行训练做准备--------------------')
+
+df=spark.read.csv('movie_ratings_df.csv',inferSchema=True,header=True)   # 读取电影评分数据
+
+print((df.count(),len(df.columns)))             # 查看数据规模
+df.printSchema()                                # 数据列信息
+
+df.orderBy(rand()).show(10,False)               # 查看数据
+
+print('-------------- 进行数据转换,主要将类别数据，转换为可通过数值来度量------------------')
+
+from pyspark.ml.feature import StringIndexer,IndexToString              # StringIndexer可以把字符串的列按照出现频率进行排序，将字符串转化为可度量的
+
+stringIndexer = StringIndexer(inputCol="title", outputCol="title_new")  # 构建StringIndexer对象，设定输入列和输出列
+
+model = stringIndexer.fit(df)                                           # 构建model模型
+
+indexed = model.transform(df)                                           # 使用模型转换数据
+
+indexed.show(10)
+
+indexed.groupBy('title_new').count().orderBy('count',ascending=False).show(10,False)    # 查看分类的数据样式
+
+train,test=indexed.randomSplit([0.75,0.25])                             # 划分训练数据和测试数据
+
+print('--------------- 使用推荐模型相关的库 ------------------------')
+
+from pyspark.ml.recommendation import ALS
+
